c300d51940515768cd6b1ed331fddf19bc54fb0b
wisnuadaow <wisnuadaow@gmail.com>
2021-09-06 01:21:38 +0700

    tugas mod 1 blm selesai

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = 30; //Membuat variabel a
+const kali = () =>{ // Arrow function
+    return 1 * a + 10; // Mengembalikan nilai
+}
+console.log(kali()); // Menampilkan hasil pada console
+nilai = kali(true); // Mmebuat objek nilai
+hasil = nilai >=80 ? "Nilai A": // Pengkondisian dalam bentuk ternary operator
+        nilai >=70 ? "Nilai B":
+        nilai >=60 ? "Nilai C":
+        nilai >=50 ? "Nilai D":
+        "Nilai E";
+console.log(hasil); // Menampilkan hasil pada console
+
+
+
+
